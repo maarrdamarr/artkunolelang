c237f65ca8a4a84ea43945442e887c94ca77a08a
maarrdamarr <damardarma42@gmail.com>
2025-11-27 16:58:01 +0700

    update27

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,27 +17,32 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User biasa
-        User::factory()->create([
+        User::updateOrCreate(['email' => 'user@test.com'], [
             'name' => 'User Test',
-            'email' => 'user@test.com',
+            'email_verified_at' => now(),
             'password' => bcrypt('password'),
             'role' => 'user',
         ]);
 
         // Penjual
-        User::factory()->create([
+        User::updateOrCreate(['email' => 'penjual@test.com'], [
             'name' => 'Penjual Test',
-            'email' => 'penjual@test.com',
+            'email_verified_at' => now(),
             'password' => bcrypt('password'),
             'role' => 'penjual',
         ]);
 
         // Admin
-        User::factory()->create([
+        User::updateOrCreate(['email' => 'admin@test.com'], [
             'name' => 'Admin Test',
-            'email' => 'admin@test.com',
+            'email_verified_at' => now(),
             'password' => bcrypt('password'),
             'role' => 'admin',
+        ]);
+
+        $this->call([
+            AddLandingSectionsSeeder::class,
+            AddTestItemsSeeder::class,
         ]);
     }
 }
